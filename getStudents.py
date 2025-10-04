18b82a53d13efc98824c0258743949338cc921eb
Siri G B <sirigb25@gmail.com>
2025-10-04 20:42:18 +0530

    Update getStudents.py

--- getStudents.py
+++ getStudents.py
@@ -2,20 +2,19 @@ import json
 import boto3
 
 def lambda_handler(event, context):
-    # Initialize a DynamoDB resource object for the specified region
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-2')
-
-    # Select the DynamoDB table named 'studentData'
+    # Use the correct region
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
     table = dynamodb.Table('studentData')
 
-    # Scan the table to retrieve all items
+    data = []
     response = table.scan()
-    data = response['Items']
+    data.extend(response.get('Items', []))
 
-    # If there are more items to scan, continue scanning until all items are retrieved
     while 'LastEvaluatedKey' in response:
         response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-        data.extend(response['Items'])
+        data.extend(response.get('Items', []))
 
-    # Return the retrieved data
-    return data
+    return {
+        'statusCode': 200,
+        'body': json.dumps(data)
+    }
